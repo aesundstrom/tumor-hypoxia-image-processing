2a075644dedd0575bc603cd4d958d392f90e572c
Andrew Sundstrom <aes@acm.org>
2015-07-22 15:09:05 -0400

    Added test_intensity_sample_ray_bundles.m.

---
+++ test_intensity_sample_ray_bundles.m
@@ -0,0 +1,46 @@
+% Elda-1: high-concentration anti-pimo; no stratification; all 20x
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1L-2_s-q-1-20.tif',  'Elda-1_PIMO-1L-2_s-q-1-20',  [960 556 1390], [590 1278 1758]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1L-2_s-q-2-20.tif',  'Elda-1_PIMO-1L-2_s-q-2-20',  [844], [1188]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1L-2_s-q-3-20.tif',  'Elda-1_PIMO-1L-2_s-q-3-20',  [1438 1304 1180 1050 968], [1310 1256 1206 1118 986]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1L-2_s-q-4-20.tif',  'Elda-1_PIMO-1L-2_s-q-4-20',  [1352], [1774]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1L-2_s-q-5-20.tif',  'Elda-1_PIMO-1L-2_s-q-5-20',  [], []);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1L-2_s-q-6-20.tif',  'Elda-1_PIMO-1L-2_s-q-6-20',  [1536], [580]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1L-2_s-q-7-20.tif',  'Elda-1_PIMO-1L-2_s-q-7-20',  [78 854 1510 1766], [626 168 314 1244]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1L-2_s-q-8-20.tif',  'Elda-1_PIMO-1L-2_s-q-8-20',  [1396 1174 346], [2024 336 1082]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1L-2_s-q-9-20.tif',  'Elda-1_PIMO-1L-2_s-q-9-20',  [], []);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1L-2_s-q-10-20.tif', 'Elda-1_PIMO-1L-2_s-q-10-20', [1582], [966]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1R-2_s-q-1-20.tif',  'Elda-1_PIMO-1R-2_s-q-1-20',  [656 646 674 648], [722 1656 1904 2102]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1R-2_s-q-2-20.tif',  'Elda-1_PIMO-1R-2_s-q-2-20',  [702 670 684 840 1018 1312 1420 1590 1742], [918 1138 1282 1350 1426 1742 1860 1970 2046]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1R-2_s-q-3-20.tif',  'Elda-1_PIMO-1R-2_s-q-3-20',  [46 204], [1396 1478]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1R-2_s-q-4-20.tif',  'Elda-1_PIMO-1R-2_s-q-4-20',  [1210], [1668]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1R-2_s-q-5-20.tif',  'Elda-1_PIMO-1R-2_s-q-5-20',  [806 1298 1596], [1004 1568 1568]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1R-2_s-q-6-20.tif',  'Elda-1_PIMO-1R-2_s-q-6-20',  [822 1574 1342], [1010 1452 1008]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1R-2_s-q-7-20.tif',  'Elda-1_PIMO-1R-2_s-q-7-20',  [848 408 376], [1264 1572 628]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1R-2_s-q-8-20.tif',  'Elda-1_PIMO-1R-2_s-q-8-20',  [1166 838], [792 2356]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1R-2_s-q-9-20.tif',  'Elda-1_PIMO-1R-2_s-q-9-20',  [536 1632 18], [296 1508 2126]);
+intensity_sample_ray_bundles('/path/to/data/Elda-1_PIMO-1R-2_s-q-10-20.tif', 'Elda-1_PIMO-1R-2_s-q-10-20', [1329 1350 978], [327 1005 2085]);
+
+% Andrew-3: low-concentration anti-pimo; stratification criteria: (1) one or more complete lesion at 10x or 20x, with one or more vessels; (2) 80% viable + 1 or more vessels; 10x and 20x
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1L-6_bot-2-1-1-10.tif', 'Andrew-3_PIMO-1L-6_bot-2-1-1-10', [1563 1410 654 500 864 534 308 106], [1905 2066 1544 1650 1014 794 682 494]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1L-6_bot-2-1-1-20.tif', 'Andrew-3_PIMO-1L-6_bot-2-1-1-20', [1209 915 639], [1053 1389 1521]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1L-6_bot-2-1-2-10.tif', 'Andrew-3_PIMO-1L-6_bot-2-1-2-10', [1401 1236 358 472 380 180 828 1058 1268 1432 1696], [1782 1899 1826 334 768 668 756 932 1062 1160 1318]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1L-6_bot-2-1-2-20.tif', 'Andrew-3_PIMO-1L-6_bot-2-1-2-20', [741 1590], [1254 1758]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1L-6_bot-2-1-3-10.tif', 'Andrew-3_PIMO-1L-6_bot-2-1-3-10', [1413 1195 394 630], [1698 1762 2382 606]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1L-6_bot-2-1-3-20.tif', 'Andrew-3_PIMO-1L-6_bot-2-1-3-20', [1038 579], [1242 1359]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1L-6_bot-4-1-1-10.tif', 'Andrew-3_PIMO-1L-6_bot-4-1-1-10', [660 1042 1236], [914 1316 1696]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1L-6_bot-4-1-1-20.tif', 'Andrew-3_PIMO-1L-6_bot-4-1-1-20', [210 711 1119], [369 1125 1911]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1L-6_top-2-1-1-10.tif', 'Andrew-3_PIMO-1L-6_top-2-1-1-10', [501 588 1254 1515 812], [384 795 852 957 1816]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1L-6_top-2-1-2-10.tif', 'Andrew-3_PIMO-1L-6_top-2-1-2-10', [1068 1554 150 1332 1260 1248 369 360], [483 678 330 1197 1512 1752 1359 1995]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1L-6_top-2-1-3-10.tif', 'Andrew-3_PIMO-1L-6_top-2-1-3-10', [728], [1550]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1L-6_top-4-1-1-10.tif', 'Andrew-3_PIMO-1L-6_top-4-1-1-10', [722 996 1194], [1408 1724 2086]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1R-6_bot-1-1-1-10.tif', 'Andrew-3_PIMO-1R-6_bot-1-1-1-10', [472], [1936]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1R-6_bot-2-1-1-10.tif', 'Andrew-3_PIMO-1R-6_bot-2-1-1-10', [984], [1176]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1R-6_bot-3-1-1-10.tif', 'Andrew-3_PIMO-1R-6_bot-3-1-1-10', [1506 1474 1266 1046 844 676], [292 676 880 1316 1252 1256]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1R-6_bot-3-1-2-10.tif', 'Andrew-3_PIMO-1R-6_bot-3-1-2-10', [1173 1468 1082 1018], [408 1682 1908 1758]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1R-6_bot-4-1-1-10.tif', 'Andrew-3_PIMO-1R-6_bot-4-1-1-10', [729 1098 422], [1128 528 1250]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1R-6_bot-4-1-2-10.tif', 'Andrew-3_PIMO-1R-6_bot-4-1-2-10', [1474 1170 912], [1156 1278 1232]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1R-6_top-1-1-1-10.tif', 'Andrew-3_PIMO-1R-6_top-1-1-1-10', [1464 1044 630], [282 1668 1920]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1R-6_top-2-1-1-10.tif', 'Andrew-3_PIMO-1R-6_top-2-1-1-10', [348 567 1005 1350 188 543], [573 1263 1029 777 1746 2043]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1R-6_top-2-1-2-10.tif', 'Andrew-3_PIMO-1R-6_top-2-1-2-10', [1629 1070 624 528], [984 1239 974 1902]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1R-6_top-3-1-1-10.tif', 'Andrew-3_PIMO-1R-6_top-3-1-1-10', [496 436 914 1218 1184 708 530], [1250 1552 1290 1178 850 650 224]);
+intensity_sample_ray_bundles('/path/to/data/Andrew-3_PIMO-1R-6_top-4-1-1-10.tif', 'Andrew-3_PIMO-1R-6_top-4-1-1-10', [1593 1389 1146 786 423 1431], [639 887 1332 1266 1458 1821]);
